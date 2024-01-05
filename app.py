de302205db719b55fa17cb4904778c365efcc12e
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-01-05 12:46:11 -0500

    display error message

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
@@ -21,6 +20,7 @@ login_manager.login_view = 'login'
 # Configuration for SQLAlchemy with MySQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 
@@ -29,7 +29,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-
+    energy_data = db.relationship('EnergyData', back_populates='user')
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -38,6 +38,20 @@ class User(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class EnergyData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    consumption = db.Column(db.Float, nullable=False)  
+    energy_type = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  
+
+    user = db.relationship('User', back_populates='energy_data')
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'EnergyData': EnergyData}
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -69,12 +83,16 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user)
             return redirect(url_for('home'))
+        else:
+            flash('Invalid email or password', 'error') 
     return render_template('login.html', form=form)
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
-@app.route('/some-protected-route')
+
 
 @app.route('/profile')
 @login_required
